4b55d213322444de77e79c486617824049e895a7
SFuwka <Alexjavac@yandex.ru>
2021-07-19 15:23:42 +0300

    publish dev fix

--- src/features/axiosSettings.js
+++ src/features/axiosSettings.js
@@ -1,8 +1,13 @@
 import * as axios from 'axios'
 
-const axiosInstance = axios.create({
-    withCredentials: true,
-    baseURL: `${process.env.REACT_APP_API_ADRESS}/api/`
-})
+const axiosInstance = process.env.NODE_ENV === 'production'
+    ? axios.create({
+        withCredentials: true,
+        baseURL: `${process.env.REACT_APP_API_ADRESS}/api/`
+    })
+    : axios.create({
+        withCredentials: true,
+        baseURL: `${process.env.REACT_APP_API_ADRESS_DEV}/api/`
+    })
 
 export default axiosInstance
